07b31d9884f04b2d9a115fb65e99c90af2b97cda
Ilya Puzyrev <puzyrev-im@yandex.ru>
2026-03-13 05:50:35 +0300

    migrate: use gotgbot types for Telegram Update
    - Replace map[string]any with gotgbot.Update for better expr support
    - Update route conditions to use Go field names (Message, From, etc.)
    - Update tests and documentation
    - Add gotgbot/v2 dependency

--- telegram_types.go
+++ telegram_types.go
@@ -1,13 +1,7 @@
 package main
 
-// User represents a Telegram user or bot
-type User struct {
-	ID        int64  `json:"id"`
-	IsBot     bool   `json:"is_bot"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name,omitempty"`
-	Username  string `json:"username,omitempty"`
-}
+import (
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
 
-// Update represents an incoming update from Telegram as a map
-type Update map[string]any
+type Update = gotgbot.Update
